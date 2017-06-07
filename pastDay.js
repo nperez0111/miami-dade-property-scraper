33ac44f21886c59090f36e51cac3d2a94ce67af0
Nick The Sick <nperez0111@gmail.com>
2017-06-07 15:01:11 -0400

    got file naming correct

--- pastDay.js
+++ pastDay.js
@@ -1,7 +1,7 @@
 'use strict';
 const { folio, search, get, titles, excel } = require( './utils' ),
     excelbuilder = require( 'msexcel-builder-protobi' ),
-    workbook = excelbuilder.createWorkbook( './', ( "sample" || ( new Date() ).toDateString() ) + '.xlsx' )
+    workbook = excelbuilder.createWorkbook( './', ( ( new Date() ).toDateString() ) + '.xlsx' )
 
 
 
@@ -10,7 +10,16 @@ if ( !module.parent ) {
 }
 
 function single( startDate, endDate, title ) {
-    return excel.run( workbook, { startDate: startDate, endDate: endDate }, title )
+    const isSameAsTitles = Array.isArray( title ) && title.length == titles.length
+    let extra = ""
+    if ( !isSameAsTitles ) {
+        if ( !Array.isArray( title ) ) {
+            title = [ title ]
+        }
+        extra = " " + title.join( " " )
+    }
+    const fileName = startDate.toDateString() + ' - ' + endDate.toDateString() + extra
+    return excel.run( excelbuilder.createWorkbook( './', fileName + '.xlsx' ), { startDate: startDate, endDate: endDate }, title )
 }
 module.exports = function ( startDate, endDate ) {
     return single( startDate, endDate, titles )
